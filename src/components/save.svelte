6dc6337a61c1ab4ef78e46d2b7a892c030f30fa5
usamahbass <basalamahusamah7@gmail.com>
2020-11-02 10:34:46 +0700

    change to icons

--- src/components/save.svelte
+++ src/components/save.svelte
@@ -10,10 +10,27 @@
     bottom: 30%;
     right: 5%;
   }
+
+  svg {
+    margin: 0 auto;
+  }
 </style>
 
 <button
-  class={`fixed bg-white rounded-full shadow-xl ${$appTheme === "gelap" ? "text-black" : "text-black"} focus:outline-none overflow-hidden hover:bg-black hover:text-white`}
+  class={`fixed bg-white rounded-full shadow-xl ${$appTheme === 'gelap' ? 'text-black' : 'text-black'} focus:outline-none overflow-hidden hover:bg-black hover:text-white`}
   on:click={save}>
-  SIMPAN
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-save"><path
+      d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
+    <polyline points="17 21 17 13 7 13 7 21" />
+    <polyline points="7 3 7 8 15 8" /></svg>
 </button>
